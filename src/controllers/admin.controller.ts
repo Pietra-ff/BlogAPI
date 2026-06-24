584ed816511d506848554a5512bdbe4492f68582
Pietra-ff <ferreirapietra380@gmail.com>
2026-06-24 08:41:15 -0300

    Iniciando cadastro de post

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,21 +1,54 @@
-import { RequestHandler } from "express"
+import { RequestHandler, Response } from "express"
+import { ExtendedRequest } from '../types/extended-request'
+import z from 'zod'
+import { getUserById } from "../services/user.service"
+import { handleCover } from "../services/post.service"
 
-export const addPost: RequestHandler = async (req, res) => {
-		// Lógica para adicionar um novo post
+export const addPost = async (req: ExtendedRequest, res: Response) => {
+	if (!req.user) return
+}
+export const editPost: RequestHandler = async (req, res) => {
+	if (!req.user) return
+
+	const schema = z.object({
+		title: z.string(),
+		tags: z.string(),
+		body: z.string()
+	})
+	const data = schema.safeParse(req.body)
+	if (!data.success) {
+		return res.status(400).json({ error: data.error.flatten().fieldErros })
+	}
+
+	if (!req.file) {
+		return res.status(400).json({ error: 'image is required' })
+	}
+
+	const coverName = await handleCover(req.file)
+	if (!coverName) {
+		return res.status(400).json({ error: 'Invalid cover image' })
+	}
+}
+
+
+
+
+export const editPost: RequestHandler = async (req, res) => {
+	// Lógica para adicionar um novo post
 }
 
 export const editPost: RequestHandler = async (req, res) => {
-		// Lógica para editar um post existente
+	// Lógica para editar um post existente
 }
 
 export const deletePost: RequestHandler = async (req, res) => {
-		// Lógica para deletar um post
+	// Lógica para deletar um post
 }
 
 export const getAllPosts: RequestHandler = async (req, res) => {
-		// Lógica para obter todos os posts
+	// Lógica para obter todos os posts
 }
 
 export const getPost: RequestHandler = async (req, res) => {
-		// Lógica para obter um post específico
+	// Lógica para obter um post específico
 }
